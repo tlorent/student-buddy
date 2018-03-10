2ba7aee3e43400dd9266cadcff068122b71ccc84
Tim <tim.lorent@icloud.com>
2018-03-10 17:02:23 +0100

    Added mixins.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 // Take the styles.css file, pipe it through a number of PostCSS functions, bring it to its new destination.
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     // If an error happens
     // The watch task will not see an error, it will see that the styles task has ended like it normally would
     .on('error', function(errorInfo) {
