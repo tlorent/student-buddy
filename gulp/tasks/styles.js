8ccf0857d28d039c5290cefae08552f2c6ce3666
Tim <tim.lorent@icloud.com>
2018-03-16 14:14:08 +0100

    Finished styling header.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,20 +1,26 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import'),
-mixins = require('postcss-mixins');
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer'),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested'),
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins'),
+	hexrgba = require('postcss-hexrgba');
 
 // Take the styles.css file, pipe it through a number of PostCSS functions, bring it to its new destination.
-gulp.task('styles', function(){
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
-    // If an error happens
-    // The watch task will not see an error, it will see that the styles task has ended like it normally would
-    .on('error', function(errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit('end');
-    })
-    .pipe(gulp.dest('./app/temp/styles'));
-})
+gulp.task('styles', function() {
+	return (
+		gulp
+			.src('./app/assets/styles/styles.css')
+			.pipe(
+				postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer])
+			)
+			// If an error happens
+			// The watch task will not see an error, it will see that the styles task has ended like it normally would
+			.on('error', function(errorInfo) {
+				console.log(errorInfo.toString());
+				this.emit('end');
+			})
+			.pipe(gulp.dest('./app/temp/styles'))
+	);
+});
